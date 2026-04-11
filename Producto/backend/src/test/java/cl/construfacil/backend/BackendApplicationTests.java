2372dfd6c939e67749f446422ebe72ec63d1ab8e
consuelojerezv <co.jerezv@duocuc.cl>
2026-04-10 20:12:20 -0400

    primer commit

---
+++ Producto/backend/src/test/java/cl/construfacil/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.construfacil.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
